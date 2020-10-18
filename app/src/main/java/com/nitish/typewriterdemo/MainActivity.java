5bda8a0e25fffded1ae1313fa138fc78d7db0ff5
Nitish Gadangi <nitishgadangi@gmail.com>
2020-10-18 19:59:59 +0530

    update demo app

--- app/src/main/java/com/nitish/typewriterdemo/MainActivity.java
+++ app/src/main/java/com/nitish/typewriterdemo/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.nitish.typewriterview.TypeWriterView;
+
 public class MainActivity extends AppCompatActivity {
+
+    TypeWriterView typeWriterView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        typeWriterView = findViewById(R.id.typeWriterView);
+        typeWriterView.animateText("Hello this is a typewriter textview");
     }
 }
